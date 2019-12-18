e9c77b02e7e3fc0cb91ace930473e8bd15f1a6f7
Ben <bjbraham@gmail.com>
2019-12-19 08:01:00 +1100

    Fix spacing issues

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,13 +4,13 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 
 const Navbar: React.FC = () => {
-	return (
-			<AppBar position="static">
-				<Toolbar>
-						Home
-				</Toolbar>
-			</AppBar>
-	)
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        Home
+      </Toolbar>
+    </AppBar>
+   )
 }
 
 export default Navbar
