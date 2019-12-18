313796adc1fd149251afe6f3d7efaba92d8997d6
Ben <bjbraham@gmail.com>
2019-12-19 07:30:31 +1100

    Improve style for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css'
-
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-
-import Home from './pages/home/Home'
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+			<Navbar />
       <Router>
         <Switch>
           <Route exact path="/">
