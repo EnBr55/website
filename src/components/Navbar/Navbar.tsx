be783b0dfd4e3d6a20b9724fd0026ef61b6816f6
Ben <bjbraham@gmail.com>
2019-12-19 07:29:31 +1100

    Create Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
+const Navbar: React.FC = () => {
+	return (
+			<AppBar position="static">
+				<Toolbar>
+						Home
+				</Toolbar>
+			</AppBar>
+	)
+}
+
+export default Navbar
