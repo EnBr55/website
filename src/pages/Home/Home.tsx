1cbfbea0a6cd6ee994e3c172d91a8f44d0996223
Ben <bjbraham@gmail.com>
2019-12-21 10:43:07 +1100

    Better visualise placeholder material

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     <div className="Home">
       <h1>Placeholder</h1>
       <div className="Bio">
-        <h2>Some subheading</h2>
+        <h2>About me</h2>
         <p>
           Etiam ac hendrerit leo. Ut rhoncus enim pellentesque nisl vestibulum
           ullamcorper. Vivamus vel ante consequat libero lobortis pretium at at
@@ -17,6 +17,9 @@ const Home: React.FC = () => {
           suscipit justo, quis pretium nunc augue sed metus. Vestibulum
           pellentesque ipsum at purus ultricies, sit amet feugiat elit
           pellentesque. Donec justo eros, tempor nec semper vitae, fringilla
+        </p>
+        <h2>Technical Skills</h2>
+        <p>
           consectetur dolor. Donec hendrerit magna sodales placerat bibendum.
           Curabitur maximus est a arcu efficitur hendrerit. Fusce dolor erat,
           porttitor ac consequat vel, porttitor in lectus. Integer fringilla
@@ -24,19 +27,26 @@ const Home: React.FC = () => {
           finibus id nulla sed, placerat lobortis nisl. Phasellus at aliquet
           turpis, fermentum gravida libero. Quisque vel nibh tellus. Mauris
           auctor cursus nisl, ut consectetur nisl tempus a. Quisque interdum
-          eleifend odio at maximus. Vestibulum nec consectetur metus, at
-          tristique nisi. Integer nec posuere lectus, ac dictum orci.
-          Suspendisse tristique ipsum a egestas congue. Proin sagittis rhoncus
-          dapibus. Curabitur aliquet erat eu ullamcorper egestas. In vulputate
-          bibendum massa elementum dapibus. Mauris pellentesque dolor vel
-          fringilla posuere. Vestibulum dolor lorem, mollis at malesuada nec,
-          vehicula in neque. Nam hendrerit ligula a lacus efficitur semper.
-          Phasellus finibus rutrum augue accumsan sagittis. Curabitur id purus
-          sed magna molestie varius luctus id nibh. Proin sagittis, est eu
-          congue condimentum, dui leo pretium sapien, vel vestibulum libero
-          mauris tempor mauris. Aliquam libero erat, pharetra a rhoncus a,
-          maximus eget ex. Duis non accumsan magna. Sed tincidunt, felis
-          fringilla facilisis egestas, dolor augue condimentum turpis, eu
+          eleifend odio at maximus.
+        </p>
+        <h2>Projects</h2>
+        <p>
+          Vestibulum nec consectetur metus, at tristique nisi. Integer nec
+          posuere lectus, ac dictum orci. Suspendisse tristique ipsum a egestas
+          congue. Proin sagittis rhoncus dapibus. Curabitur aliquet erat eu
+          ullamcorper egestas. In vulputate bibendum massa elementum dapibus.
+          Mauris pellentesque dolor vel fringilla posuere. Vestibulum dolor
+          lorem, mollis at malesuada nec, vehicula in neque. Nam hendrerit
+          ligula a lacus efficitur semper. Phasellus finibus rutrum augue
+          accumsan sagittis.
+        </p>
+        <h2>Contact</h2>
+        <p>
+          Curabitur id purus sed magna molestie varius luctus id nibh. Proin
+          sagittis, est eu congue condimentum, dui leo pretium sapien, vel
+          vestibulum libero mauris tempor mauris. Aliquam libero erat, pharetra
+          a rhoncus a, maximus eget ex. Duis non accumsan magna. Sed tincidunt,
+          felis fringilla facilisis egestas, dolor augue condimentum turpis, eu
           convallis dui elit sodales lacus. Suspendisse et libero pharetra,
           bibendum velit ac, sagittis velit. Pellentesque gravida placerat dui
           vel bibendum. Proin auctor, massa sed tempus pretium, velit augue
