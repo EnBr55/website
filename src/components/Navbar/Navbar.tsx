0a81744f1bcec959423459fab3bf730356456419
Ben <bjbraham@gmail.com>
2019-12-23 09:57:29 +1100

    Add logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import NavbarItem from '../NavbarItem/NavbarItem'
 import { BrowserRouter as Router, Redirect } from 'react-router-dom'
+import logo from '../../bb.png'
 
 const Navbar: React.FC = () => {
   const [redirect, setRedirect] = React.useState<string | undefined>(undefined)
@@ -10,6 +11,9 @@ const Navbar: React.FC = () => {
       {redirect !== undefined && <Redirect to={redirect} />}
       <NavbarItem text='Home' redirectLocation='' setRedirect={setRedirect} />
       <NavbarItem text='Projects' redirectLocation='projects' setRedirect={setRedirect} />
+        <div className='logo-container' onClick={() => setRedirect('')}>
+        <img src={logo} alt='logo' height='100%'/>
+      </div>
     </div>
    )
 }
