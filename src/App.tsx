64f5dcda18dc9e3b92e00156d54f2d6fdbc221f5
Ben <bjbraham@gmail.com>
2019-12-23 11:56:01 +1100

    Adjust content size to remove overflow

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,24 @@ import Navbar from './components/Navbar/Navbar'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route>
-            <div>
-              <h1>404 - Page not Found :(</h1>
-              <a href={'https://benbraham.com'}>Go home</a>
-            </div>
-          </Route>
-        </Switch>
+        <div className='header'>
+          <Navbar />
+        </div>
+        <div className='content'>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route>
+              <div>
+                <h1>404 - Page not Found :(</h1>
+                <a href={'https://benbraham.com'}>Go home</a>
+              </div>
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </div>
   )
