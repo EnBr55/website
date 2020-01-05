2e5984000bc055b2c3dcd8af18c8fa7209a6018d
Ben <bjbraham@gmail.com>
2020-01-06 08:49:54 +1100

    Add react-p5-wrapper and create type declarations

---
+++ src/pages/Snakes/Snakes.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Snakes.css'
+import P5Wrapper from 'react-p5-wrapper'
+import { SnakeSketch } from './SnakeSketch'
+
+const Snakes: React.FC = () => {
+  return (
+    <div className='snakes'>
+      <P5Wrapper sketch={SnakeSketch} />
+    </div>
+  )
+}
