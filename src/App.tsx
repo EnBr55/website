69c15c5b97cfaedfe3cefba5eabde77025d5dc48
Ben <bjbraham@gmail.com>
2020-01-05 13:29:46 +1100

    Add local storage for theme

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,20 @@ import {ThemeContext, themes} from './contexts/ThemeContext'
 
 const App: React.FC = () => {
   const [themeState, setThemeState] = React.useState({
-    theme: themes.light,
+    // check if theme has been previously set, otherwise default to light
+    //   this will just become a switch case if more themes are eventually added
+    theme: window.localStorage.getItem('theme') === 'dark' ? themes.dark : themes.light,
     toggleTheme: () => {
+      window.localStorage.setItem(
+        'theme', themeState.theme === themes.dark ? 'light' : 'dark'
+      )
       // we use the second form of setState (passing a function)
       //   so as to force the state updated to be received synchronously
       setThemeState(themeState => ({
         ...themeState,
         theme: themeState.theme === themes.light ? themes.dark : themes.light,
       }))
+      console.log(window.localStorage)
     }
   })
   return (
